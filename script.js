1261a423f8ebd62afeb9ff69644913b3281ed29e
Shawn Xue <98925039+ShawnXue10969@users.noreply.github.com>
2024-11-19 21:30:44 +0800

    post launch update

--- script.js
+++ script.js
@@ -52,7 +52,10 @@ fetch("./answers.json")
         }
       }
 
-      return bestMatch || "Sorry, I don't quite understand your question. Please try again or give us a phone call +61 0406 888 546.";
+      return (
+        bestMatch ||
+        "Sorry, I don't quite understand your question. Please try again or give us a phone call +61 0406 888 546."
+      );
     };
 
     const handleChat = () => {
@@ -86,6 +89,21 @@ fetch("./answers.json")
     });
   });
 
+const serviceCards = document.getElementsByClassName("service-card");
+for (let card of serviceCards) {
+  let overlay = card.lastElementChild;
+  let close = card.querySelector(".close-btn");
+
+  overlay.addEventListener("click", () => {
+    card.classList.toggle("show-detail");
+    document.body.classList.toggle("no-scroll");
+  });
+  close.addEventListener("click", () => {
+    card.classList.toggle("show-detail");
+    document.body.classList.toggle("no-scroll");
+  });
+}
+
 $(".owl-carousel").owlCarousel({
   items: 1,
   loop: true,
@@ -101,4 +119,33 @@ $(".owl-carousel").owlCarousel({
       items: 3,
     },
   },
+});
+
+$(".scrollable").on("DOMMouseScroll mousewheel", function (ev) {
+  var $this = $(this),
+    scrollTop = this.scrollTop,
+    scrollHeight = this.scrollHeight,
+    height = $this.height(),
+    delta =
+      ev.type == "DOMMouseScroll"
+        ? ev.originalEvent.detail * -40
+        : ev.originalEvent.wheelDelta,
+    up = delta > 0;
+
+  var prevent = function () {
+    ev.stopPropagation();
+    ev.preventDefault();
+    ev.returnValue = false;
+    return false;
+  };
+
+  if (!up && -delta > scrollHeight - height - scrollTop) {
+    // Scrolling down, but this will take us past the bottom.
+    $this.scrollTop(scrollHeight);
+    return prevent();
+  } else if (up && delta > scrollTop) {
+    // Scrolling up, but this will take us past the top.
+    $this.scrollTop(0);
+    return prevent();
+  }
 });
